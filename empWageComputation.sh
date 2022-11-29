9a786b1fa21556b9fee51997d1188055a3f4c32d
SakharePooja <poojasakhare94@gmail.com>
2022-11-29 22:23:32 +0530

    Added:usingCaseStatement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,19 +4,21 @@ isPresent=$((RANDOM%3));
 perHourWage=20;
 day_Hr=8;
 
-if [ $isPresent -eq 0 ]
-then
-	echo "Empolyee is absent";
-	workingHour=0;
+case $isPresent in
+	0)  
+		echo "Empolyee is absent";
+		workingHour=0;
+	;;
 
-elif [ $isPresent -eq 1 ]
-then
-	echo "Empolyee is present";
-	workingHour=$day_Hr;
-else 
-	echo "Empolyee is working as part time";
-	workingHour=$(( $day_Hr/2 ));
-fi
+	1)
+		echo "Empolyee is present";
+		workingHour=$day_Hr;
+	;;
+	
+	*)
+		echo "Empolyee is working as part time";
+		workingHour=$(( $day_Hr/2 ));
+esac
 
 wage=$(($perHourWage * $workingHour));
 echo "Empolyee has earned $wage today";
