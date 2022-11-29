15d4bc81fc3cab77149468da6191c7a6bae93c41
SakharePooja <poojasakhare94@gmail.com>
2022-11-29 20:50:25 +0530

    Added:EmployyeWage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 isPresent=$((RANDOM%3));
+perHourWage=20;
+workingHour=8;
+
 if [ $isPresent -eq 0 ]
 then
 	echo "Empolyee is absent";
+	workingHour=0;
+
 elif [ $isPresent -eq 1 ]
 then
 	echo "Empolyee is present";
+	workingHour=8;
 else 
 	echo "Empolyee is working as part time";
+	workingHour=4;
 fi
+
+wage=$(($perHourWage * $workingHour));
+echo "Empolyee has earned $wage $ today";
